d29c23b4ee95cbe6a30923a56b3160f4953af247
catfo <pranavlsgreat210120@gmail.com>
2023-10-19 13:28:42 +0530

    feat : sent files pane completed(front end)

---
+++ app/components/SentFilePane.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// Will be changed when backend is implemented
+type SentFilePaneProps = {
+    sentTo : string,
+    fileName: string,
+    time: string,
+}
+
+
+export default function SentFilePane(props: SentFilePaneProps) {
+  return (
+    <div title={props.time} className='w-[80%] h-[150px] my-5 flex justify-center items-center bg-slate-350 dark:bg-slate-700 hover:opacity-90 hover:dark:bg-slate-650 rounded-lg'>
+        <div className='content flex w-[90%] h-[90%] items-center text-3xl divide-x-4 divide-slate-600'>
+            <div className='w-1/3 h-full flex items-center justify-center'>
+                <div className='w-28 h-28 bg-slate-400 dark:bg-slate-500 rounded-full flex justify-center items-center'>TXT</div>
+            </div>
+            <div className='w-1/3 h-full flex items-center justify-center'>
+                {props.fileName}
+            </div>
+            <div className='w-1/3 h-full flex items-center justify-center'>
+                {props.sentTo}
+            </div>
+        </div>
+    </div>
+  )
+}
--- app/components/SentFilesPane.tsx
+++ app/components/SentFilesPane.tsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import SentFilePane from './SentFilePane'
+
+// Will be removed when backend is implemented
+const dummyFileArray = [
+  {
+    sentTo:"catfo",
+    fileName:"dummy.txt",
+    time:"dummytime",
+  },
+  {
+    sentTo:"catfo",
+    fileName:"dummy.txt",
+    time:"dummytime",
+  },
+  {
+    sentTo:"catfo",
+    fileName:"dummy.txt",
+    time:"dummytime",
+  },
+  {
+    sentTo:"catfo",
+    fileName:"dummy.txt",
+    time:"dummytime",
+  },
+  {
+    sentTo:"catfo",
+    fileName:"dummy.txt",
+    time:"dummytime",
+  },
+  
+]
+
 
 export default function SentFilesPane() {
   return (
     <div className='viewSentFiles w-1/2 flex justify-center items-center'>
-        <div className="bg-[#D6DEE8] dark:bg-[#162032] w-[90%] h-[90%] rounded-3xl flex justify-center items-center text-5xl md:text-7xl">
-        <div className='content py-4 px-4 w-full h-full'>
-
+        <div className="bg-[#D6DEE8] dark:bg-[#162032] w-[90%] h-[90%] rounded-3xl flex justify-center text-5xl md:text-7xl">
+        <div className='flex flex-col content py-4 px-4 w-full h-full items-center'>
+          <div className='w-fit h-fit'>Files Sent</div>
+          <div className='flex flex-col w-full h-[580px] items-center overflow-scroll bg-slate-300 dark:bg-slate-800 mt-5 mb-10 rounded-3xl'>
+            {dummyFileArray.map((fileProps) => {return <SentFilePane {...fileProps} /> })}
+          </div>
         </div>
         </div>
     </div>
