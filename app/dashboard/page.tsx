985a7e90efe613f19c895cc20f219e651279de8f
catfo <pranavlsgreat210120@gmail.com>
2023-10-19 11:32:48 +0530

    fix : bug fix, deployment error(2)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       router.replace("/")
       return
     }
-    userName = localStorage.getItem("user")?
+    userName = localStorage.getItem("user")!
   }
   catch (e){
   }
