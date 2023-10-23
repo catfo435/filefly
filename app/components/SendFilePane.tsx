65ff9bc46ca5862af3969d553ea45a0770305c2d
catfo <pranavlsgreat210120@gmail.com>
2023-10-23 21:18:30 +0530

    fix! : send files page made responsive

--- app/components/SendFilePane.tsx
+++ app/components/SendFilePane.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function SendFilePane() {
   return (
-    <div className='sendFiles w-1/2 flex justify-center items-center'>
-        <div className="bg-[#D6DEE8] dark:bg-[#162032] w-[90%] h-[90%] rounded-3xl flex justify-center items-center text-4xl md:text-4xl">
-          <div className='content py-4 px-4 w-full h-full flex flex-col items-center'>
+    <div className='sendFiles w-full md:w-1/2 flex flex-auto md:flex-none justify-center items-center'>
+        <div className="bg-[#D6DEE8] dark:bg-[#162032] w-[90%] h-[90%] rounded-3xl flex justify-center items-center text-4xl">
+          <div className='content py-4 px-4 w-full h-full flex flex-col items-center shrink-0'>
             <span className='text-6xl'>Send File</span>
             <span className='text-4xl mt-5'>To</span>
             <input placeholder='Username' className='w-2/3 h-fit rounded-xl my-8 px-3 py-3 dark:bg-slate-700 outline-none' type='text'></input>
--- app/components/SentFilesPane.tsx
+++ app/components/SentFilesPane.tsx
@@ -39,7 +39,7 @@ const dummyFileArray = [
 
 export default function SentFilesPane() {
   return (
-    <div className='viewSentFiles w-1/2 flex justify-center items-center'>
+    <div className='viewSentFiles w-1/2 hidden md:flex justify-center items-center'>
         <div className="bg-[#D6DEE8] dark:bg-[#162032] w-[90%] h-[90%] rounded-3xl flex justify-center text-5xl md:text-7xl">
         <div className='flex flex-col content py-4 px-4 w-full h-full items-center'>
           <div className='w-fit h-fit'>Files Sent</div>
