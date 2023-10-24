ef9426cff874921b66c9a5b3390116bca0319c34
catfo <pranavlsgreat210120@gmail.com>
2023-10-25 02:20:28 +0530

    feat! : sign up completed, login user check done

---
+++ app/backend/index.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/firestore";
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import { doc, getDoc } from "firebase/firestore"
+
+
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CREDS)
+
+const app = initializeApp(firebaseConfig)
+
+export const firebaseDB  = getFirestore(app);
